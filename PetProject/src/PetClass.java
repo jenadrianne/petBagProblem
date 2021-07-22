3f6e3db5413392f22d85e5ed92cc93e19bf3ea5d
Jennifer Adrianne I. Caballes <yan4414@gmail.com>
2021-07-22 16:24:00 +0800

    Add files via upload

--- PetProject/src/PetClass.java
+++ PetProject/src/PetClass.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class PetClass {
 	
@@ -6,10 +9,12 @@ public class PetClass {
 	private String petType; 
 	private String petName; 
 	private int petAge; 
-	private int dogSpaces = 30;
-	private int catSpaces = 12;
+	private static Map<String , PetClass> dogSpaces = new HashMap<String, PetClass>(); 
+	private static Map<String , PetClass> CatSpaces = new HashMap<String, PetClass>();
 	private int daysStay;
 	private double amountDue;
+	//add unique value 
+	private int petId; 
 
 	//Constructors
 	public PetClass() {
@@ -23,8 +28,15 @@ public class PetClass {
 	}
 	
 	//Accessors and Mutators
+	
 	public String getPetType() {
 		return petType;
+	}
+	public int getPetId() {
+		return petId;
+	}
+	public void setPetId(int petId) {
+		this.petId = petId;
 	}
 	public void setPetType(String petType) {
 		this.petType = petType;
@@ -41,17 +53,17 @@ public class PetClass {
 	public void setPetAge(int petAge) {
 		this.petAge = petAge;
 	}
-	public int getDogSpaces() {
+	public static Map<String, PetClass> getDogSpaces() {
 		return dogSpaces;
 	}
-	public void setDogSpaces(int dogSpaces) {
-		this.dogSpaces = dogSpaces;
+	public static void setDogSpaces(Map<String, PetClass> dogSpaces) {
+		PetClass.dogSpaces = dogSpaces;
 	}
-	public int getCatSpaces() {
-		return catSpaces;
+	public static Map<String, PetClass> getCatSpaces() {
+		return CatSpaces;
 	}
-	public void setCatSpaces(int catSpaces) {
-		this.catSpaces = catSpaces;
+	public static void setCatSpaces(Map<String, PetClass> catSpaces) {
+		CatSpaces = catSpaces;
 	}
 	public int getDaysStay() {
 		return daysStay;
@@ -65,6 +77,79 @@ public class PetClass {
 	public void setAmountDue(double amountDue) {
 		this.amountDue = amountDue;
 	}
-
 	
+	
+	/**
+	 * Pseudocode : 
+	 * 	1. 	Determine the Type of the pet.
+		2. 	If Dog, get the total spaces available for a dog.
+			If Cat, get the total spaces available for a cat.
+		3. 	If the available spaces is greater than zero , perform the next procedure : 
+			3.1	Check if the pet is a returning customer
+				3.1.1 If the pet is a returning customer , update the existing record
+				3.1.2 If the pet is a new customer, create a new pet record.
+			3.2 Ask the customer , the pet's length of stay in the Bag.
+				3.2.1 If the pet's stay for 2 or more days, asks the customer if they want the pet to be groomed. 
+					3.2.1.1 If the customer agrees, add the information in the record
+			3.3 Assign a Place for the pet
+			3.4 Update the spaces available by incrementing it when a pet record is added or updated.
+	 * @param petType
+	 * @param petName
+	 * @param petAge
+	 * @param daysStay
+	 */
+	public static void PetCheckIn(String petType , String petName, int petAge, int daysStay) {
+		
+		int spaceAvailable = 0; 
+		boolean newCustomer = true; 
+		PetClass petInfo = null; 
+		Map<String , PetClass> cage = null;
+		
+		if(petType.equals("Dog")) {
+			petInfo = new DogClass(petType , petName, petAge, daysStay); 
+			cage =  PetClass.getDogSpaces(); 
+			spaceAvailable = 30 - cage.size();
+			//30 is the default number of cages for dogs;
+		} else {
+			petInfo = new CatClass(petType , petName, petAge, daysStay); 
+			cage = PetClass.getCatSpaces();
+			spaceAvailable = 12 - cage.size();
+			//12 is the default number of cages for cats;
+		}
+		
+		if (spaceAvailable > 0) {
+			/* check if returning customer 
+			 * When using Map.put if the key does not exist, then the Pet is added. 
+			 * If it exist it is updated. 
+			 * However you have to change the key to a more unique variable since 
+			 * pets can have the same name
+			*/
+			cage.put(petName, petInfo);
+			
+			if(petType.equals("Dog") && daysStay > 2) {
+				// ask the user if pet is for grooming. 
+				System.out.println("Would you like for a grooming? (Yes/No) "); 
+				Scanner scan = new Scanner(System.in); 
+				String answer = scan.next(); 
+				scan.close();
+				if(answer.equalsIgnoreCase("Yes")) {
+					((DogClass) petInfo).setGrooming(true);
+				}
+				
+				((DogClass) petInfo).setDogSpaceNumber(spaceAvailable);
+			}
+			
+			int petindex =0;
+			for(String pet : cage.keySet()) {
+				petindex++;
+				if(pet.equals(petName)) { break; }
+			}
+			if(petType.equals("Dog")) {
+				((DogClass) petInfo).setDogSpaceNumber(petindex);
+			}else {
+				((CatClass) petInfo).setCatSpaceNumber(petindex); 
+			}
+		}
+		
+	}
 }
---
+++ PetProject/src/PetCounterClass.java
@@ -0,0 +1,43 @@
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class PetCounterClass {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PetClass dogInfo = new DogClass (); 
+		PetClass catInfo = new CatClass ();
+		PetClass dogInfo2 = new DogClass ();
+		PetClass dogInfo3 = new DogClass ();
+		
+		dogInfo.PetCheckIn("Dog", "Loki", 2, 3);
+		catInfo.PetCheckIn("Cat", "Sisa", 1, 1);
+		dogInfo2.PetCheckIn("Dog", "Taco", 2, 1);
+		dogInfo3.PetCheckIn("Dog", "Loki", 5, 1); // update
+		
+		printCage();
+	}
+	
+	public static void printCage() {
+		Map<String, PetClass> dogCage = PetClass.getDogSpaces(); 
+		Map<String, PetClass> catCage = PetClass.getCatSpaces(); 
+		
+		System.out.println("==========================================="); 
+
+		System.out.println("Dog Cage"); 
+		for(Entry<String, PetClass> dog : dogCage.entrySet()) {
+			DogClass entry = (DogClass) dog.getValue();
+			System.out.format("%d - %s, %s, %d, %d \n", entry.getDogSpaceNumber(), entry.getPetType(), entry.getPetName(), entry.getPetAge(), entry.getDaysStay());
+		}
+		
+		System.out.println("==========================================="); 
+		
+		System.out.println("Cat Cage"); 
+		for(Entry<String, PetClass> dog : catCage.entrySet()) {
+			CatClass entry = (CatClass) dog.getValue();
+			System.out.format("%d - %s, %s, %d, %d \n", entry.getCatSpaceNumber(), entry.getPetType(), entry.getPetName(), entry.getPetAge(), entry.getDaysStay());
+		}
+		
+		System.out.println("==========================================="); 
+	}
+}
